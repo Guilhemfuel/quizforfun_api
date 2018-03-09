b3b5ecf61639d0011f15ab5f0fe09f181082f483
benjamina <b.berteaubenjamin@gmail.com>
2018-03-09 13:46:45 +0100

    Maj des fixtures avec la modif des entités

--- src/AppBundle/DataFixtures/ORM/LoadGamesData.php
+++ src/AppBundle/DataFixtures/ORM/LoadGamesData.php
@@ -11,13 +11,13 @@ class LoadGamesData implements FixtureInterface
     public function load(ObjectManager $manager)
     {
         $games = array(
-            array('code' => 'XF5Y', 'nbPlayerMin' => 2, 'nbPlayerMax' => 10, 'description' => 'Quiz culture'),
+            array('code' => 'XF5Y', 'nbPlayerMin' => 2, 'nbPlayerMax' => 10, 'description' => 'Quiz culture', 'isStarted' => 0, 'isFinished' => 0),
         );
 
         $players = array(
-            array('name' => 'Jean', 'owner' => false, 'score' => '2'),
-            array('name' => 'Louis', 'owner' => false, 'score' => '0'),
-            array('name' => 'Risitas', 'owner' => true, 'score' => '4')
+            array('name' => 'Jean', 'owner' => false, 'fingerprint' => 'fdsio88fezge54her', 'score' => '2'),
+            array('name' => 'Louis', 'owner' => false, 'fingerprint' => 'apioy9846rbdfjr2', 'score' => '0'),
+            array('name' => 'Risitas', 'owner' => true, 'fingerprint' => 'fzetzeyiuypdfx', 'score' => '4')
         );
 
         foreach($games as $key => $g) {
@@ -26,12 +26,15 @@ class LoadGamesData implements FixtureInterface
             $game->setNbPlayerMin($g['nbPlayerMin']);
             $game->setNbPlayerMax($g['nbPlayerMax']);
             $game->setDescription($g['description']);
+            $game->setIsStarted($g['isStarted']);
+            $game->setIsFinished($g['isFinished']);
 
             foreach($players as $p) {
                 $player = new Player();
                 $player->setName($p['name']);
                 $player->setOwner($p['owner']);
                 $player->setScore($p['score']);
+                $player->setFingerprint($p['fingerprint']);
                 $player->setGame($game);
 
                 $manager->persist($player);
